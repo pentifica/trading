30e940902ef5da66b29f55b9da98fa0083bbb26a
Rusty Fleming <russjen@swbell.net>
2023-08-22 14:44:12 -0500

    Added Fix Encoder

--- src/parser/fix/Encode.h
+++ src/parser/fix/Encode.h
@@ -29,191 +29,211 @@
 #include    <sys/time.h>
 
 namespace pentifica::trd::fix {
-/// @brief Encode a fix message.
-class Encode {
-public:
-    Encode() = default;
-    /// @brief Initialize a new fix message
-    /// @param msg_type The fix message type
-    /// @param version  The version of the fix message
-    /// @param begin    Start of the bufer where the fix message will be encoded
-    /// @param end      End+1 of the fix message buffer
-    explicit Encode(MsgType msg_type, Version version, Byte* begin, Byte* end);
-    Encode(Encode const&) = default;
-    Encode(Encode&&) = default;
-    ~Encode() = default;
-    Encode& operator=(Encode const&) = default;
-    Encode& operator=(Encode&&) = default;
-    /// @brief Returns the current length of the encoded message
-    /// @return The current length of the encoded message
-    std::size_t Size() const { return next_-begin_; }
-    /// @brief Returns the capacity of the message buffer
-    /// @return The capacity of the message buffer
-    std::size_t Capacity() const { return end_ - begin_; }
-    /// @brief Append a tag=value to the end of the fix message under construction
-    /// @tparam ValueType The data type of the value
-    /// @param tag The tag for the fix field
-    /// @param value The value for the fix field
-    template<typename ValueType>
-    void Append(Tag tag, ValueType value) {
-        BeginTag(tag);
-        Append(value);
-        EndTag();
-    }
-    /// @brief Append a tag=?? to the end of the fix message under construction.
-    /// @param tag The tag for the fix tag/=value entry
-    /// @param width The width of the value entry
-    /// @param place_holder Indicates where the value is to be placed
-    void Append(Tag tag, TagWidth width, Byte*& place_holder) {
-        BeginTag(tag);
-        place_holder = next_;
-        next_ += static_cast<std::size_t>(width);
-        EndTag();
-    }
-    /// @brief Finalize the fix message by:
-    ///        -# Filling in header information
-    ///        -# Appending the checksum
-    /// @param seq_number The message sequence number
-    /// @param last_seq_number The sequence number of the last message processed
-    void Finalize(uint32_t seq_number, uint32_t last_seq_number);
-
-private:
-    /// @brief Add a tag start to the message buffer
-    /// @param tag The tag to add
-    void BeginTag(Tag tag) {
-        AppendUnsigned(static_cast<std::underlying_type_t<Tag>>(tag));
-        Append('=');
-    }
-    /// @brief Add a tag terminator to the message buffer
-    void EndTag() {
-        *next_ = SOH;
-        ++next_;
-        ++checksum_;
-    }
-    /// @brief Update the message contents
-    /// @param value The update value
-    void Append(MsgType value) {
-        *next_ = static_cast<Byte>(value);
-        ++next_;
-        checksum_ += static_cast<decltype(checksum_)>(value);
-    }
-    void Append(uint16_t value) { AppendUnsigned(value); }
-    void Append(uint32_t value) { AppendUnsigned(value); }
-    void Append(uint64_t value) { AppendUnsigned(value); }
-    void Append(int16_t value) { AppendSigned(value); }
-    void Append(int32_t value) { AppendSigned(value); }
-    void Append(int64_t value) { AppendSigned(value); }
-    void Append(char const* value) {
-        for(; *value != '\0'; value++) Append(static_cast<Byte>(*value));
-    }
-    void Append(std::string const& value) {
-        for(auto& x : value) Append(static_cast<Byte>(x));
-    }
-    void Append(struct tm tm);
-    void Append(struct timeval value);
-    void Append(double value, double digits = 6.0);
-    /// @brief Update the message contents with the unsigned value
-    /// @tparam T The value type
-    /// @param value The value to format
-    template<typename T>
-    void AppendUnsigned(T value) {
-        static_assert(std::is_same_v<decltype(value), uint16_t>
-            || std::is_same_v<decltype(value), uint32_t>
-            || std::is_same_v<decltype(value), uint64_t>, "Must be an unsigned value");
-        constexpr T ten{10};
-        auto begin = next_;
-        do {
-            Append(static_cast<Byte>(value % ten) + '0');
-            value /= ten;
-        }   while(value != 0);
-
-        auto end = next_ - 1;
-
-        for(auto end = next_ - 1; begin < end; ++begin, ++end) {
-            std::swap(*begin, *end);
+    /// @brief Encode a fix message.
+    class Encode {
+    public:
+        Encode() = default;
+        /// @brief Initialize a new fix message
+        /// @param msg_type The fix message type
+        /// @param version  The version of the fix message
+        /// @param begin    Start of the bufer where the fix message will be encoded
+        /// @param end      End+1 of the fix message buffer
+        explicit Encode(MsgType msg_type, Version version, Byte* begin, Byte* end);
+        Encode(Encode const&) = default;
+        Encode(Encode&&) = default;
+        ~Encode() = default;
+        Encode& operator=(Encode const&) = default;
+        Encode& operator=(Encode&&) = default;
+        /// @brief Returns the current length of the encoded message
+        /// @return The current length of the encoded message
+        std::size_t Size() const { return next_-begin_; }
+        /// @brief Returns the capacity of the message buffer
+        /// @return The capacity of the message buffer
+        std::size_t Capacity() const { return end_ - begin_; }
+        /// @brief Append a tag=value to the end of the fix message under construction
+        /// @tparam ValueType The data type of the value
+        /// @param tag The tag for the fix field
+        /// @param value The value for the fix field
+        template<typename ValueType>
+        void Append(Tag tag, ValueType&& value) {
+            BeginTag(tag);
+            Append(value);
+            EndTag();
         }
-    }
-    /// @brief Update the message contents with the unsigned value front-padding with '0'
-    /// @tparam T The unsigned value type
-    /// @param value The value to format
-    /// @param width The width to format
-    template<typename T>
-    void AppendUnsigned(T value, uint32_t width) {
-        static_assert(std::is_same_v<decltype(value), uint16_t>
-            || std::is_same_v<decltype(value), uint32_t>
-            || std::is_same_v<decltype(value), uint64_t>, "Must be an unsigned value");
-        constexpr T ten{10};
-
-        auto begin = next_;
-        next_ += width;
-        auto end = next_;
-        std::fill(begin, end, '0');
-
-        while(--end > begin) {
-            *end += static_cast<Byte>(value % ten);
-            checksum_ += static_cast<uint32_t>(*end);
-            value /= ten;
+        template<typename ValueType>
+        void Append(Tag tag, ValueType&& value, uint32_t width) {
+            BeginTag(tag);
+            Append(value, width);
+            EndTag();
         }
-    }
-    /// @brief Update the message contents with the signed value
-    /// @tparam T The signed value type
-    /// @param value The value to format
-    template<typename T>
-    void AppendSigned(T value) {
-        static_assert(std::is_same_v<decltype(value), int16_t>
-            || std::is_same_v<decltype(value), int32_t>
-            || std::is_same_v<decltype(value), int64_t>, "Must be a signed value");
-        if(value < 0) {
-            Append('-');
-            value = std::abs(value);
+        /// @brief Finalize the fix message by:
+        ///        -# Filling in header information
+        ///        -# Appending the checksum
+        /// @param seq_number The message sequence number
+        /// @param last_seq_number The sequence number of the last message processed
+        void Finalize(uint32_t seq_number, uint32_t last_seq_number);
+    
+    private:
+        /// @brief Append a tag=?? to the end of the fix message under construction.
+        /// @param tag The tag for the fix tag/=value entry
+        /// @param width The width of the value entry
+        /// @param place_holder Indicates where the value is to be placed
+        void Append(Tag tag, TagWidth width, Byte*& place_holder) {
+            BeginTag(tag);
+            place_holder = next_;
+            next_ += static_cast<std::size_t>(width);
+            EndTag();
         }
-        if constexpr(std::is_same_v<decltype(value), int32_t>)
-            AppendUnsigned(static_cast<uint32_t>(value));
-        else
-            AppendUnsigned(static_cast<uint64_t>(value));
-    }
-    /// @brief Update the message contents with the signed value front-padding with '0'
-    /// @tparam T The signed value type
-    /// @param value The value to format
-    /// @param width The width to format
-    template<typename T>
-    void AppendSigned(T value, TagWidth width) {
-        static_assert(std::is_same_v<decltype(value), int16_t>
-            || std::is_same_v<decltype(value), int32_t>
-            || std::is_same_v<decltype(value), int64_t>, "Must be a signed value");
-        if(value < 0) {
-            Append('-');
-            value = std::abs(value);
+        /// @brief Add a tag start to the message buffer
+        /// @param tag The tag to add
+        void BeginTag(Tag tag) {
+            AppendUnsigned(static_cast<std::underlying_type_t<Tag>>(tag));
+            Append('=');
         }
-        if constexpr(std::is_same_v<decltype(value), int32_t>)
-            AppendUnsigned(static_cast<uint32_t>(value, width));
-        else
-            AppendUnsigned(static_cast<uint64_t>(value, width));
-    }
-    template<typename T>
-    void InsertUnsigned(Byte* begin, TagWidth width, T value) {
-        auto save_next = next_;
-        next_ = begin;
-        AppendUnsigned(value, static_cast<std::underlying_type_t<TagWidth>>(value));
-        next_ = save_next;
-    }
-    template<typename T>
-    void InsertSigned(Byte* begin, TagWidth width, T value) {
-        auto save_next = next_;
-        next_ = begin;
-        AppendSigned(value, width);
-        next_ = save_next;
-    }
-private:
-    Byte* begin_{};
-    Byte* end_{};
-    Byte* next_{};
-    Byte* msg_type_{};
-    Byte* body_length_field_{};
-    Byte* body_length_field_end_{};
-    Byte* msg_seq_num_field_{};
-    Byte* sending_time_field_{};
-    Byte* last_msg_seq_num_processed_field_{};
-    std::uint32_t checksum_{};
-};  
+        /// @brief Add a tag terminator to the message buffer
+        void EndTag() {
+            *next_ = SOH;
+            ++next_;
+            ++checksum_;
+        }
+        void AppendByte(Byte byte) {
+            *next_ = byte;
+            ++next_;
+            checksum_ += static_cast<decltype(checksum_)>(byte);
+        }
+        /// @brief Update the message contents
+        /// @param value The update value
+        void Append(MsgType value) { AppendByte(static_cast<Byte>(value)); }
+        void Append(char value) { AppendByte(static_cast<Byte>(value)); }
+        void Append(uint16_t value) { AppendUnsigned(value); }
+        void Append(uint32_t value) { AppendUnsigned(value); }
+        void Append(uint64_t value) { AppendUnsigned(value); }
+        void Append(unsigned long long value) { AppendUnsigned(value); }
+        void Append(int16_t value) { AppendSigned(value); }
+        void Append(int32_t value) { AppendSigned(value); }
+        void Append(int64_t value) { AppendSigned(value); }
+        void Append(long long value) { AppendSigned(value); }
+        void Append(uint16_t value, uint32_t width) { AppendUnsigned(value, width); }
+        void Append(uint32_t value, uint32_t width) { AppendUnsigned(value, width); }
+        void Append(uint64_t value, uint32_t width) { AppendUnsigned(value, width); }
+        void Append(unsigned long long value, uint32_t width) { AppendUnsigned(value, width); }
+        void Append(int16_t value, uint32_t width) { AppendSigned(value, width); }
+        void Append(int32_t value, uint32_t width) { AppendSigned(value, width); }
+        void Append(int64_t value, uint32_t width) { AppendSigned(value, width); }
+        void Append(long long value, uint32_t width) { AppendSigned(value, width); }
+        void Append(char const* value) {
+            for(; *value != '\0'; value++) AppendByte(static_cast<Byte>(*value));
+        }
+        void Append(std::string const& value) {
+            for(auto& x : value) AppendByte(static_cast<Byte>(x));
+        }
+        void Append(struct tm tm);
+        void Append(struct timeval value);
+        void Append(double value, double digits = 6.0);
+        /// @brief Update the message contents with the unsigned value
+        /// @tparam T The value type
+        /// @param value The value to format
+        template<typename T>
+        void AppendUnsigned(T value) {
+            static_assert(std::is_same_v<decltype(value), uint16_t>
+                || std::is_same_v<decltype(value), uint32_t>
+                || std::is_same_v<decltype(value), uint64_t>
+                || std::is_same_v<decltype(value), unsigned long long>, "Must be an unsigned value");
+            constexpr T ten{10};
+            auto begin = next_;
+            do {
+                AppendByte(static_cast<Byte>(value % ten) + '0');
+                value /= ten;
+            }   while(value != 0);
+    
+            for(auto end = next_ - 1; begin < end; ++begin, --end) {
+                std::swap(*begin, *end);
+            }
+        }
+        /// @brief Update the message contents with the unsigned value front-padding with '0'
+        /// @tparam T The unsigned value type
+        /// @param value The value to format
+        /// @param width The width to format
+        template<typename T>
+        void AppendUnsigned(T value, uint32_t width) {
+            static_assert(std::is_same_v<decltype(value), uint16_t>
+                || std::is_same_v<decltype(value), uint32_t>
+                || std::is_same_v<decltype(value), uint64_t>
+                || std::is_same_v<decltype(value), unsigned long long>, "Must be an unsigned value");
+            constexpr T ten{10};
+    
+            auto begin = next_;
+            next_ += width;
+            auto end = next_;
+            std::fill(begin, end, '0');
+    
+            while(--end > begin) {
+                *end += static_cast<Byte>(value % ten);
+                checksum_ += static_cast<uint32_t>(*end);
+                value /= ten;
+            }
+        }
+        /// @brief Update the message contents with the signed value
+        /// @tparam T The signed value type
+        /// @param value The value to format
+        template<typename T>
+        void AppendSigned(T value) {
+            static_assert(std::is_same_v<decltype(value), int16_t>
+                || std::is_same_v<decltype(value), int32_t>
+                || std::is_same_v<decltype(value), int64_t>
+                || std::is_same_v<decltype(value), long long>, "Must be a signed value");
+            if(value < 0) {
+                Append('-');
+                value = std::abs(value);
+            }
+            if constexpr(std::is_same_v<decltype(value), int32_t>)
+                AppendUnsigned(static_cast<uint32_t>(value));
+            else
+                AppendUnsigned(static_cast<uint64_t>(value));
+        }
+        /// @brief Update the message contents with the signed value front-padding with '0'
+        /// @tparam T The signed value type
+        /// @param value The value to format
+        /// @param width The width to format
+        template<typename T>
+        void AppendSigned(T value, uint32_t width) {
+            static_assert(std::is_same_v<decltype(value), int16_t>
+                || std::is_same_v<decltype(value), int32_t>
+                || std::is_same_v<decltype(value), int64_t>
+                || std::is_same_v<decltype(value), long long>, "Must be a signed value");
+            if(value < 0) {
+                Append('-');
+                value = std::abs(value);
+            }
+            if constexpr(std::is_same_v<decltype(value), int32_t>)
+                AppendUnsigned(static_cast<uint32_t>(value, width));
+            else
+                AppendUnsigned(static_cast<uint64_t>(value, width));
+        }
+        template<typename T>
+        void InsertUnsigned(Byte* begin, TagWidth width, T value) {
+            auto save_next = next_;
+            next_ = begin;
+            AppendUnsigned(value, static_cast<std::underlying_type_t<TagWidth>>(width));
+            next_ = save_next;
+        }
+        template<typename T>
+        void InsertSigned(Byte* begin, TagWidth width, T value) {
+            auto save_next = next_;
+            next_ = begin;
+            AppendSigned(value, width);
+            next_ = save_next;
+        }
+    private:
+        Byte* begin_{};
+        Byte* end_{};
+        Byte* next_{};
+        Byte* msg_type_{};
+        Byte* body_length_field_{};
+        Byte* body_length_field_end_{};
+        Byte* msg_seq_num_field_{};
+        Byte* sending_time_field_{};
+        Byte* last_msg_seq_num_processed_field_{};
+        std::uint32_t checksum_{};
+    };  
 }
--- tests/parser/fix/Test_Encode.cpp
+++ tests/parser/fix/Test_Encode.cpp
@@ -2,6 +2,132 @@
 
 #include    <gtest/gtest.h>
 
-TEST(Test_Encode, ctor) {
+#include    <array>
+#include    <iostream>
+#include    <string_view>
+#include    <tuple>
+#include    <utility>
 
+namespace {
+    template<typename T>
+    void Dump(T&& begin, std::size_t length) {
+        for(; length != 0; --length, begin++) {
+            if(*begin == pentifica::trd::fix::SOH) std::cerr << '|';
+            else std::cerr << *begin;
+        }
+        std::cerr << '\n';
+    }
+}
+
+TEST(Test_Encode, wrapper) {
+    using namespace pentifica::trd::fix;
+    std::array<Byte, 256> buffer;
+
+    Encode message(MsgType::LOGON, Version::_4_2, buffer.begin(), buffer.end());
+    message.Finalize(789, 790);
+
+    std::string_view message_view((char*) buffer.begin(), (char*) (buffer.begin() + message.Size()));
+
+    constexpr std::size_t overhead{19 + 6};
+    constexpr std::size_t body{57};
+    EXPECT_EQ(overhead + body, static_cast<std::size_t>(message.Size()));
+
+    EXPECT_EQ(message_view[0], '8');
+    EXPECT_TRUE(message_view.find("=FIX.4.2"));
+    EXPECT_TRUE(message_view.find("9=000057"));
+    EXPECT_TRUE(message_view.find("35=A"));
+    EXPECT_TRUE(message_view.find("34=000000789"));
+    EXPECT_TRUE(message_view.find("369=000000790"));
+    EXPECT_TRUE(message_view.find("10="));
+}
+
+namespace {
+    using namespace pentifica::trd::fix;
+
+    template<typename TupleType, std::size_t ...Is>
+    void TestAppend(TupleType const& tp, std::index_sequence<Is...>) {
+        auto field_test = [](auto const& test_data) {
+            std::array<Byte, 256> buffer;
+            Encode message(MsgType::LOGON, Version::_4_2, buffer.begin(), buffer.end());
+            auto const& [value, expected] = test_data;
+            message.Append(Tag::RawData, value);
+            message.Finalize(1, 0);
+            std::string_view message_view((char*) buffer.begin(), (char*) (buffer.begin() + message.Size()));
+            EXPECT_TRUE(message_view.find(expected));
+        };
+
+        (field_test(std::get<Is>(tp)), ...);
+    }
+}
+
+TEST(Test_Encode, test_append) {
+    using namespace pentifica::trd::fix;
+
+    auto test_data = std::tuple {
+        std::pair {short(23),                       "96=23"},
+        std::pair {(unsigned short)(35),            "96=35"},
+        std::pair {int(57),                         "96=57"},
+        std::pair {(unsigned int)(98),              "96=98"},
+        std::pair {long(42),                        "96=42"},
+        std::pair {(unsigned long)(84),             "96=84"},
+        std::pair {(long long)(73),                 "96=73"},
+        std::pair {(unsigned long long)(36),        "96=36"},
+        std::pair {int16_t(91),                     "96=91"},
+        std::pair {int32_t(-37),                    "96=-37"},
+        std::pair {int64_t(-34928),                 "96=-34928"},
+        std::pair {uint16_t(4),                     "96=4"},
+        std::pair {uint32_t(3482),                  "96=3482"},
+        std::pair {uint64_t(23873901),              "96=23873901"},
+        std::pair {double(34.85),                   "96=34.85"},
+        std::pair {double(-25.68),                  "96=-25.68"},
+        std::pair {char('g'),                       "96=g"},
+        std::pair {(char*)("this is test one"),     "96=this is test one"},
+        std::pair {std::string("this is test two"), "96=this is test two"}
+        };
+
+    std::size_t const size = std::tuple_size<decltype(test_data)>::value;
+    TestAppend(test_data, std::make_index_sequence<size>{});
+}
+
+namespace {
+    using namespace pentifica::trd::fix;
+
+    template<typename TupleType, std::size_t ...Is>
+    void TestAppendWidth(TupleType const& tp, std::index_sequence<Is...>) {
+        auto field_test = [](auto const& test_data) {
+            std::array<Byte, 256> buffer;
+            Encode message(MsgType::LOGON, Version::_4_2, buffer.begin(), buffer.end());
+            auto& [width, value, expected] = test_data;
+            message.Append(Tag::RawData, value, width);
+            message.Finalize(1, 0);
+            std::string_view message_view((char*) buffer.begin(), (char*) (buffer.begin() + message.Size()));
+            EXPECT_TRUE(message_view.find(expected));
+        };
+
+        (field_test(std::get<Is>(tp)), ...);
+    }
+}
+
+TEST(Test_Encode, test_append_width) {
+    using namespace pentifica::trd::fix;
+
+    auto test_data = std::tuple {
+        std::tuple {10, short(23),                       "96=0000000023"},
+        std::tuple {5,  (unsigned short)(35),            "96=00035"},
+        std::tuple {8,  int(57),                         "96=00000057"},
+        std::tuple {11, (unsigned int)(98),              "96=00000000098"},
+        std::tuple {7,  long(42),                        "96=0000042"},
+        std::tuple {12, (unsigned long)(84),             "96=000000000084"},
+        std::tuple {20, (long long)(73),                 "96=00000000000000000073"},
+        std::tuple {11, (unsigned long long)(36),        "96=00000000036"},
+        std::tuple {7,  int16_t(91),                     "96=0000091"},
+        std::tuple {23, int32_t(-37),                    "96=-0000000000000000000037"},
+        std::tuple {14, int64_t(-34928),                 "96=-0000000034928"},
+        std::tuple {8,  uint16_t(4),                     "96=0004"},
+        std::tuple {19, uint32_t(3482),                  "96=0000000000000003482"},
+        std::tuple {22, uint64_t(23873901),              "96=0000000000000023873901"}
+        };
+
+    std::size_t const size = std::tuple_size<decltype(test_data)>::value;
+    TestAppendWidth(test_data, std::make_index_sequence<size>{});
 }
